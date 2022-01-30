2990261b74d23bf99b8d5bab72f2bae0273b6b0a
Shinya Fujino <shf0811@gmail.com>
2022-01-30 14:34:09 +0900

    Lesson 2

--- script.ts
+++ script.ts
@@ -2,9 +2,12 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-// A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
+  const result = await prisma.user.findMany({
+    skip: 2,
+    take: 2,
+  })
+  console.log(result)
 }
 
 main()
